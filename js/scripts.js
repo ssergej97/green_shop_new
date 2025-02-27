af13bc2a885f523b1a83c43369896557443fd2bf
ssergej97 <sergejsubota1997@gmail.com>
2025-02-27 11:36:03 +0200

    carousel

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+    autoplayTimeout: 6500,
+    autoplaySpeed: 1000,
+    loop: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        1100:{
+            items:2
+        }
+    }
+
+})
